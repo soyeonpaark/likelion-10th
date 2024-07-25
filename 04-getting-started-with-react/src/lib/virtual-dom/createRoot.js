61be67771f3a3ac3fcdc5224a4e14b2e97353ffd
yamoo9 <yamoo9@naver.com>
2024-07-25 13:17:54 +0900

    [학습] 라이브러리 모듈 추가 확장
    - isValidElement
    - 가상 요소가 맞는 지 여부를 반환하는 함수

--- 04-getting-started-with-react/src/lib/virtual-dom/createRoot.js
+++ 04-getting-started-with-react/src/lib/virtual-dom/createRoot.js
@@ -1,5 +1,7 @@
 import VirtualDomRoot from './VirtualDomRoot.js';
 
+// React DOM API 유사
+// ReactDOM.createRoot(container, options?)
 function createRoot(rootElement) {
   return new VirtualDomRoot(rootElement);
 }
