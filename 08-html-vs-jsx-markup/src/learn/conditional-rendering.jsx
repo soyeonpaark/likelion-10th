7ba5b6e2ca9793dcfd1cf1f89caf4abd4d468cd0
yamoo9 <yamoo9@naver.com>
2024-08-01 13:45:31 +0900

    [학습] PropTypes와 함께 하는 타입 검사 Part 3.
    - User Defined Types
    - Custom Types

--- 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
+++ 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
@@ -1,6 +1,5 @@
-import { oneOf } from 'prop-types';
 import { isTrueOrFalse } from '../utils';
-import { IMAGE_TYPES } from '../data/learn';
+import { ImagesType } from '../@types/types.d';
 
 // 이미지 경로 불러오기
 import kakaoTalkImagePath from '../assets/kakao-talk.svg?url';
@@ -102,5 +101,5 @@ ConditionalRendering.propTypes = {
   // 배열에 포함된 것 중 하나를 확인하는 검사
   // ['react', 'vite', 'next.js', 'kakao talk']
   // imageType: oneOf(['react', 'vite', 'next.js', 'kakao talk']),
-  imageType: oneOf(IMAGE_TYPES).isRequired,
+  imageType: ImagesType.isRequired,
 };
