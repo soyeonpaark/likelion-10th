590519c194c573b81905deebecc658c15298938a
yamoo9 <yamoo9@naver.com>
2024-08-05 15:05:38 +0900

    [학습] 확장 가능한 텍스트 컴포넌트 선언적으로 제어 Step 3
    - 클래스 컴포넌트 상태 업데이트
    - 컴포넌트 렌더 메서드 실행
    - 렌더 메서드가 반환하는 마크업
    - 실제 DOM 업데이트

--- 12-react-component-states/src/components/ExpandableText.class.jsx
+++ 12-react-component-states/src/components/ExpandableText.class.jsx
@@ -36,12 +36,14 @@ class ExpandableText extends Component {
       renderText = children.toString().slice(0, limit) + '...';
     }
 
+    const buttonLabel = isExpand ? '축소' : '확장';
+
     return (
       <div className="ExpandableText" {...restProps}>
-        <p>{renderText}</p>
+        <p>{isExpand ? children : renderText}</p>
         {isExpandable && (
           <button type="button" onClick={this.handleExpand}>
-            {isExpand ? '축소' : '확장'}
+            {buttonLabel}
           </button>
         )}
       </div>
@@ -50,6 +52,9 @@ class ExpandableText extends Component {
 
   handleExpand = () => {
     // 상태 업데이트 기능 추가
+    this.setState({
+      isExpand: !this.state.isExpand,
+    });
   };
 }
 
