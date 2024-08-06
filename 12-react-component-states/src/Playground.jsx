e01c935abefd32ec47f575659c89f65ab6f006ba
yamoo9 <yamoo9@naver.com>
2024-08-06 13:50:46 +0900

    [학습] 리액트 상태 업데이트에 대한 이해 III
    - 상태 업데이트 2가지 방법
    - 상태 업데이트는 큐 자료 구조 활용

--- 12-react-component-states/src/Playground.jsx
+++ 12-react-component-states/src/Playground.jsx
@@ -4,7 +4,7 @@ import Counter from '@/components/Counter/Counter';
 function Playground() {
   return (
     <div style={styles}>
-      <Counter min={0} count={3} max={5} />
+      <Counter min={-50} count={3} max={50} />
       {/* <UsersPage /> */}
     </div>
   );
