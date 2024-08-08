63deb17794a3914857fc7a4d49004185aed18e65
yamoo9 <yamoo9@naver.com>
2024-08-09 08:22:39 +0900

    [구성] 학습 자료
    - 14-using-react-refs

--- 14-component-test-with-vitest/src/utils/delay.js
+++
@@ -1,7 +0,0 @@
-function delay(timeout = 1000) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
-
-export default delay;
--- 14-using-react-refs/src/components/Avatar.jsx
+++ 14-using-react-refs/src/components/Avatar.jsx
@@ -1,20 +1,12 @@
-// --------------------------------------------------------------------------
-// ✅ 테스트 시나리오
-// --------------------------------------------------------------------------
-// - 컴포넌트 렌더링 관점에서 어떤 테스트를 수행할 지 고민해 작성하세요.
-// --------------------------------------------------------------------------
-
-import { number, string } from 'prop-types';
-import { StatusMessagesType } from '@/@types/entries.d';
+import { number, oneOf, string } from 'prop-types';
 
 Avatar.propTypes = {
   name: string.isRequired,
   photo: string.isRequired,
-  status: StatusMessagesType,
+  status: oneOf(['offline', 'online', 'dont-disturb', 'away']),
   size: number,
 };
 
-/**@type {({ name, photo, status, size }: { name: string, photo: string, status?: 'offline' | 'online' | 'dont-disturb' | 'away', size?: number }) => JSX.Element} */
 function Avatar({ name, photo, status = 'offline', size = 64 }) {
   let iconPath = '';
   let statusMessage = '';
