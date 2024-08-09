5a10c9c95f7d5156e342dec3dba5442e96d357d2
yamoo9 <yamoo9@naver.com>
2024-08-09 15:35:55 +0900

    [학습] 틱택토 게임 만들기 Part 11.
    - 게임 플레이어 집합
    - 상태 끌어올리기 (Squares → Board)

--- 13-tic-tac-toe-game/src/tic-tac-toe/constants.js
+++ 13-tic-tac-toe-game/src/tic-tac-toe/constants.js
@@ -3,6 +3,9 @@
 // 예: 🍟 vs. 🍔 / 🎩 vs. 🤡
 export const PLAYER = { ONE: '🍟', TWO: '🤡' };
 
+// 게임 플레이어 집합
+export const PLAYER_LIST = Object.values(PLAYER);
+
 // 게임 플레이어의 수
 export const PLAYER_COUNT = Object.keys(PLAYER).length;
 
