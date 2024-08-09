64e2c8bfbd1ffb1904d449b164ed94eeb21920fa
yamoo9 <yamoo9@naver.com>
2024-08-09 11:42:15 +0900

    [학습] 틱택토 게임 만들기 Part 6.
    - 게임 상태 업데이트
    - 화면(UI) 변경

--- 13-tic-tac-toe-game/src/tic-tac-toe/constants.js
+++ 13-tic-tac-toe-game/src/tic-tac-toe/constants.js
@@ -1,12 +1,14 @@
 // 게임에서 사용되는 말(플레이어)를 상수로 정의합니다.
 // const CONSTANT = VALUE
 // 예: 🍟 vs. 🍔 / 🎩 vs. 🤡
-export const PLAYER = {
-  ONE: '🍟',
-  TWO: '🤡',
-};
+export const PLAYER = { ONE: '🍟', TWO: '🤡' };
 
+// 게임 플레이어의 수
 export const PLAYER_COUNT = Object.keys(PLAYER).length;
 
-// 스퀘어 집합: 초기 상태 값
+// 게임판: 초기 상태 값
+// [ 0, 1, 2 ]
+// [ 3, 4, 5 ]
+// [ 6, 7, 8 ]
+// export const INITIAL_SQUARES = [null, null, null, null, null, null, null, null, null];
 export const INITIAL_SQUARES = Array(9).fill(null);
