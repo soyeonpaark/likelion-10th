8a307d57f9daba4f84a98e32017490156b25e0d5
yamoo9 <yamoo9@naver.com>
2024-08-09 10:51:22 +0900

    [학습] 틱택토 게임 만들기 Part 5.
    - 게임 상태와 파생된 상태
    - 이벤트 핸들러를 반환하는 함수 (클로저 활용)

--- 13-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
@@ -10,19 +10,20 @@
 // - [x] Square 컴포넌트의 스타일을 CSS 모듈(modules) 방식으로 설정합니다. (with Vite)
 // --------------------------------------------------------------------------
 
-import { node } from 'prop-types';
+import { func, node } from 'prop-types';
 import S from './Square.module.css';
 
 Square.propTypes = {
   children: node,
+  onPlay: func
 };
 
-function Square({ children }) {
+function Square({ children, onPlay }) {
   // [파생된 상태]
   const isDisabled = !!children;
 
   return (
-    <button className={S.component} disabled={isDisabled}>
+    <button className={S.component} onClick={onPlay} disabled={isDisabled}>
       {children}
     </button>
   );
--- 13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -1,12 +1,40 @@
-import { PLAYER } from '@/tic-tac-toe/constants';
+// --------------------------------------------------------------------------
+// ✅ 틱택토 게임 만들기 (Squares 컴포넌트)
+// --------------------------------------------------------------------------
+// - [ ] squares 배열 데이터를 게임의 상수로 설정합니다.
+// - [ ] squares 배열 데이터의 초기 상태 값은 9개의 `null`로 구성합니다.
+// - [ ] squares 배열 데이터 모듈을 불러온 후, 순환해 Sqaure 컴포넌트를 리스트 렌더링 합니다.
+// --------------------------------------------------------------------------
+
+import { useState } from 'react';
+import { PLAYER, PLAYER_COUNT, INITIAL_SQUARES } from '@/tic-tac-toe/constants';
 import Square from '../Square/Square';
+import S from './Squares.module.css';
 
 function Squares() {
+  // [게임 상태] --------------------------------------------------------------
+
+  const [squares, setSquares] = useState(INITIAL_SQUARES);
+
+  // [게임 상태 업데이트 기능] ----------------------------------------------------
+   
+  const handlePlay = (index) => () => {
+    console.log(`play game #${index}`);
+  };
+  
+  // [게임 파생된 상태] ----------------------------------------------------------
+  
+  // 게임 순서
+  const gameIndex = squares.filter(Boolean).length % PLAYER_COUNT;
+  
+  // 현재 게임 플레이어
+  const currentPlayer = gameIndex === 0 ? PLAYER.ONE : PLAYER.TWO;
+
   return (
-    <div className="Squares">
-      <Square></Square>
-      <Square>{PLAYER.ONE}</Square>
-      <Square>{PLAYER.TWO}</Square>
+    <div className={S.component}>
+      {squares.map((square, index) => {
+        return (<Square key={index} onPlay={handlePlay(index)}>{square}</Square>)
+      })}
     </div>
   );
 }
