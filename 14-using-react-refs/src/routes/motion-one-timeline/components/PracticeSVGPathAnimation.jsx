54aa98a745b6b25cbb54fcd03915cc53a13641f0
yamoo9 <yamoo9@naver.com>
2024-08-13 12:34:02 +0900

    [학습] 타임라인 애니메이션 III
    - Figma → SVG 패스 애니메이션 완성

--- 14-using-react-refs/src/routes/motion-one-timeline/components/PracticeSVGPathAnimation.jsx
+++ 14-using-react-refs/src/routes/motion-one-timeline/components/PracticeSVGPathAnimation.jsx
@@ -10,50 +10,95 @@
 //   - [x] visibility
 //   - [x] pathLength
 // --------------------------------------------------------------------------
-import { string } from 'prop-types';
+import { number, string } from 'prop-types';
 import S from './PracticeSVGPathAnimation.module.css';
+import { timeline } from 'motion';
+import { useRef } from 'react';
 
 PracticeSVGPathAnimation.propTypes = {
-  color: string,
+  strokeColor: string,
+  strokeWidth: number,
 };
 
-function PracticeSVGPathAnimation({ color = '#4729B4 ' }) {
+function PracticeSVGPathAnimation({
+  strokeColor = '#4729B4',
+  strokeWidth = 4,
+}) {
+  const svgRef = useRef(null);
+
+  const handleSVGPathAnimation = () => {
+    const { current: el } = svgRef;
+
+    const [circle1, circle2] = Array.from(el.querySelectorAll('circle'));
+    const line = el.querySelector('line');
+
+    timeline(
+      [
+        [circle1, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
+        [line, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
+        [circle2, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
+      ],
+      {
+        duration: 1.8,
+        delay: 0.2,
+        easing: 'cubic-bezier(0.79,0.14,0.15,0.86)',
+      }
+    );
+  };
+
   return (
-    <div className={S.component}>
-      <svg width={210} height={41} viewBox="0 0 210 41" fill="none">
-        <circle
-          cx="20.5"
-          cy="20.5"
-          r="17.5"
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-        />
-        <line
-          x1={35}
-          y1={20}
-          x2={173}
-          y2={20}
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-        />
-        <circle
-          cx="189.5"
-          cy="20.5"
-          r="17.5"
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-        />
-      </svg>
-    </div>
+    <>
+      <button
+        type="button"
+        className={S.button}
+        onClick={handleSVGPathAnimation}
+      >
+        SVG 패스 애니메이션
+      </button>
+
+      <div className={S.component}>
+        <svg
+          ref={svgRef}
+          width={212}
+          height={41}
+          viewBox="0 0 210 41"
+          fill="none"
+        >
+          <circle
+            cx="20.5"
+            cy="20.5"
+            r="17.5"
+            stroke={strokeColor}
+            strokeDasharray={1}
+            strokeDashoffset={0}
+            strokeWidth={strokeWidth}
+            pathLength={1}
+          />
+          <line
+            x1={36}
+            y1={20}
+            x2={173}
+            y2={20}
+            stroke={strokeColor}
+            strokeDasharray={1}
+            strokeDashoffset={0}
+            strokeWidth={strokeWidth}
+            pathLength={1}
+          />
+          <circle
+            cx="189.5"
+            cy="20.5"
+            r="17.5"
+            stroke={strokeColor}
+            strokeDasharray={1}
+            strokeDashoffset={0}
+            strokeWidth={strokeWidth}
+            pathLength={1}
+            style={{ transform: 'rotateX(180deg) rotateY(180deg)' }}
+          />
+        </svg>
+      </div>
+    </>
   );
 }
 
