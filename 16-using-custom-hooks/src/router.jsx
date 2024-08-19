b233bfb1fe2f5d98173385cbb0feab8a9479f993
yamoo9 <yamoo9@naver.com>
2024-08-19 13:28:36 +0900

    [학습] 싱글 페이지 앱 with React Router 5
    - 공용 레이아웃 (RootLayout)
    - 공용 컴포넌트 설계 (Header, Footer)

--- 16-using-custom-hooks/src/router.jsx
+++ 16-using-custom-hooks/src/router.jsx
@@ -1,14 +1,25 @@
-/* eslint-disable no-unused-vars */
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
 } from 'react-router-dom';
 
 import HomePage from '@/pages/Home';
 import NoteListPage from '@/pages/NoteList';
 import NewNotePage from '@/pages/NewNote';
 import NoteDetailPage from '@/pages/NoteDetail';
+
+// Legacy React Router v6.3-
+// const routesFromElement = createRoutesFromElements(
+//   <>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/notes" element={<NoteListPage />} />
+//     <Route path="/notes/new" element={<NewNotePage />} />
+//     <Route path="/notes/detail" element={<NoteDetailPage />} />
+//   </>
+// );
+
+// const router = createBrowserRouter(routesFromElement);
 
 // React Router v6.4+ (data APIs ✅)
 const routes = [
@@ -30,18 +41,6 @@ const routes = [
   },
 ];
 
-// const router = createBrowserRouter(routes);
-
-// Legacy React Router v6.3-
-const routesFromElement = createRoutesFromElements(
-  <>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/notes" element={<NoteListPage />} />
-    <Route path="/notes/new" element={<NewNotePage />} />
-    <Route path="/notes/detail" element={<NoteDetailPage />} />
-  </>
-);
-
-const router = createBrowserRouter(routesFromElement);
+const router = createBrowserRouter(routes);
 
 export default router;
