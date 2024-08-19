ef0757b4335329a4e606837f30052f86d798d793
yamoo9 <yamoo9@naver.com>
2024-08-19 13:09:49 +0900

    [학습] 싱글 페이지 앱 with React Router 3
    - 페이지 간 탐색 (경로(path) 매칭)
    - 매칭된 경로에 설정된 페이지 엘리먼트 렌더링

--- 16-using-custom-hooks/src/pages/Home.jsx
+++ 16-using-custom-hooks/src/pages/Home.jsx
@@ -5,13 +5,7 @@ function HomePage() {
     <div>
       <h1>홈 페이지</h1>
       <p>싱글 페이지 애플리케이션의 홈 페이지입니다.</p>
-
-      {/* <a href="/notes">노트 목록</a> */}
-      {/* <Link to={"/notes"}>노트 목록</Link> */}
-      <div style={{ display: 'flex', gap: 8 }}>
-        <Link to="/notes">노트 목록 1</Link>
-        <Link to={{ pathname: '/notes' }}>노트 목록 2</Link>
-      </div>
+      <Link to="/notes">노트 목록</Link>
     </div>
   );
 }
